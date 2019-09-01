67ff0c0812176d33b727199fc524a447868e44db
Druval Carvalho <durvalcsouza@outlook.com>
2019-09-01 13:13:50 -0300

    add images to car model

--- cars/models.py
+++ cars/models.py
@@ -5,9 +5,7 @@ class Car(models.Model):
     year = models.IntegerField()
     model = models.CharField(max_length=50)
     manufacturer = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='car_images', default='default.jpg')
 
     def __str__(self):
         return f'{self.model} {self.manufacturer} {self.year}'
-
-    # def __repr__(self):
-        # return f'{self.model} {self.manufacturer} {self.year}'
