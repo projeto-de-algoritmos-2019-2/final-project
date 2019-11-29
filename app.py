3284ad591a9a5736bafc91252ac7ab08682c1ef9
Victor Moura <victor_cmoura@hotmail.com>
2019-11-29 11:59:35 -0300

    Creates project initial structure with pygame

---
+++ app.py
@@ -0,0 +1,95 @@
+import pygame as pg
+import notifyr
+
+
+pg.init()
+screen = pg.display.set_mode((500, 720))
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
+FONT = pg.font.Font(None, 32)
+
+global_word_list = []
+
+def predict_words(wrd):
+    l = ['salve', 'opa']
+    return l
+
+def update_list():
+    h = 50
+    for word in global_word_list:
+        textsurface = FONT.render('- ' + word, True, COLOR_ACTIVE)
+        screen.blit(textsurface,(10,h))
+        h += 32
+
+class InputBox:
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pg.KEYDOWN:
+            global global_word_list
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    print(self.text)
+                    global_word_list = []
+                    self.text = ''
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                    global_word_list = predict_words(self.text)
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pg.draw.rect(screen, self.color, self.rect, 2)
+
+
+def main():
+    clock = pg.time.Clock()
+    input_box1 = InputBox(10, 10, 480, 32)
+    input_boxes = [input_box1]
+    done = False
+
+    while not done:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            for box in input_boxes:
+                box.handle_event(event)
+
+        for box in input_boxes:
+            box.update()
+
+        screen.fill((30, 30, 30))
+        for box in input_boxes:
+            box.draw(screen)
+
+        update_list()
+
+        pg.display.flip()
+        clock.tick(30)
+
+
+if __name__ == '__main__':
+    main()
+    pg.quit()
